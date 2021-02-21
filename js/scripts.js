49e5a655e67a0e9abd3799f980d218176438ca4a
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-02-21 15:35:24 -0500

    create pokemonList
    first three pokemon added with specifics

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello world');
-var favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Charmander',
+        height: '0.6',
+        types: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: '0.5',
+        types: ['water']
+    },
+    {
+        name: 'Charizard',
+        height: '1.7',
+        types: ['flying', 'fire']
+    }
+];
