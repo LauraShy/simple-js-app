ebcc4eddf8fc756b379bf57c7a79cda6c66f6b94
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-03-02 06:27:48 -0500

    update formatting for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,7 @@
 let pokemonList = [
-    {
-        name: 'Charmander',
-        height: '0.6',
-        types: ['fire']
-    },
-    {
-        name: 'Squirtle',
-        height: '0.5',
-        types: ['water']
-    },
-    {
-        name: 'Charizard',
-        height: '1.7',
-        types: ['flying', 'fire']
-    }
+  { name: 'Charmander', height: '0.6', types: ['fire'] },
+  { name: 'Squirtle', height: '0.5', types: ['water'] },
+  { name: 'Charizard', height: '1.7', types: ['flying', 'fire'] }
 ];
 
 for (let i=0; i < pokemonList.length; i++){
