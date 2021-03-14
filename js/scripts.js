d623505bfd6cb07dd9c50327f05680d48672d8f7
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-03-14 14:52:36 -0400

    IIFE update
    May need to adjust later

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,30 @@ let pokemonList = [
   { name: 'Charizard', height: '1.7', types: ['flying', 'fire'] }
 ];
 
-pokemonList.forEach(function(pokemon) {
-  if (pokemon.height <1.3 && pokemon.height >0.6){
-    document.write(pokemon.name + ", " + pokemon.height + " height, is a medium pokemon." + "<br>");
-  } else if (pokemon.height <0.7){
-    document.write(pokemon.name + ", " + pokemon.height + " height, is a small pokemon." + "<br>");
-  } else {
-    document.write(pokemon.name + ", " + pokemon.height + " height, is a large pokemon. Wow! That\'s big!" + "<br>");
-  }
-})
+let pokemonRepository = (function () {
+  let pokemonList = [];
+
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+})();
+
+pokemonRepository.add({ name: 'Weepinbell', height: '1.0', types: ['grass', 'poison']});
+console.log(pokemonRepository.getAll());
+
+(function () {
+  pokemonList.forEach(function(pokemon) {
+    if (pokemon.height <1.3 && pokemon.height >0.6){
+      document.write(pokemon.name + ", " + pokemon.height + " height, is a medium pokemon." + "<br>");
+    } else if (pokemon.height <0.7){
+      document.write(pokemon.name + ", " + pokemon.height + " height, is a small pokemon." + "<br>");
+    } else {
+      document.write(pokemon.name + ", " + pokemon.height + " height, is a large pokemon. Wow! That\'s big!" + "<br>");
+    }
+  });
+})();
