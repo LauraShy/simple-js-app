72062c43943e6050c93e0c7c88c3e544f308b2a0
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-03-14 13:49:48 -0400

    change for loop to forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,12 @@ let pokemonList = [
   { name: 'Charizard', height: '1.7', types: ['flying', 'fire'] }
 ];
 
-for (let i=0; i < pokemonList.length; i++){
-    if (pokemonList[i].height <1.3 && pokemonList[i].height >0.6){
-      document.write(pokemonList[i].name + ", " + pokemonList[i].height + " height, is a medium pokemon." + "<br>");
-    } else if (pokemonList[i].height <0.7){
-      document.write(pokemonList[i].name + ", " + pokemonList[i].height + " height, is a small pokemon." + "<br>");
-    } else {
-      document.write(pokemonList[i].name + ", " + pokemonList[i].height + " height, is a large pokemon. Wow! That\'s big!" + "<br>");
-    }
+pokemonList.forEach(function(pokemon) {
+  if (pokemon.height <1.3 && pokemon.height >0.6){
+    document.write(pokemon.name + ", " + pokemon.height + " height, is a medium pokemon." + "<br>");
+  } else if (pokemon.height <0.7){
+    document.write(pokemon.name + ", " + pokemon.height + " height, is a small pokemon." + "<br>");
+  } else {
+    document.write(pokemon.name + ", " + pokemon.height + " height, is a large pokemon. Wow! That\'s big!" + "<br>");
   }
+})
