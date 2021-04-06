05aabc2dd78366c21577513bc96cfe3ec9fa2de9
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-04-06 10:33:18 -0400

    remove objects in pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-let pokemonList = [
-  { name: 'Charmander', height: '0.6', types: ['fire'] },
-  { name: 'Squirtle', height: '0.5', types: ['water'] },
-  { name: 'Charizard', height: '1.7', types: ['flying', 'fire'] }
-];
+let pokemonList = [];
 
 //this is the IIFE where you create the pokemonRepository
 let pokemonRepository = (function () {
