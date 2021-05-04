54ce14e54875465384b3af9e8c81a74af43c119f
LauraShy <78038876+LauraShy@users.noreply.github.com>
2021-05-04 06:31:51 -0400

    add prettier code to js file

--- js/scripts.js
+++ js/scripts.js
@@ -90,3 +90,11 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
+
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+};
